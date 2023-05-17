7489b17970371a44664c9d97b86cdb39ea3390a4
Gamayun17 <127756650+Gamayun17@users.noreply.github.com>
2023-05-18 00:02:14 +0300

    Update practl27.java

--- jc1_homework/jc1_homework/src/pack4/practl27.java
+++ jc1_homework/jc1_homework/src/pack4/practl27.java
@@ -1,16 +1,19 @@
-package pack4;
+package jc1_homework.src.pack4;
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Practl27 {
-    Pattern p = Pattern.compile("[.!?,\\-]");
-    Matcher m = p.matcher("Тестовая, .строка,, с несколькими!! знаками препинания,");
-    int count = 0;
+    public static void main(String[] args) {
+
+        Pattern p = Pattern.compile("[.!?,\\-]");
+        Matcher m = p.matcher("Тестовая, .строка,, с несколькими!! знаками препинания,");
+        int count = 0;
         while (m.find()) {
-        count++;
-        System.out.println("Match number: " + count);
-        System.out.println("group() : " + m.group());
+            count++;
+            System.out.println("Match number: " + count);
+            System.out.println("group() : " + m.group());
+        }
     }
-}
 }
